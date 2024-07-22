13cf444fbec9d7eca9474335150c9b7cc626429f
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-07-22 14:10:54 +0500

    Some Changes

--- practise.cpp
+++ practise.cpp
@@ -19,6 +19,12 @@ public:
         name = n;
     }
 
+    Student(string n, int i)
+    {
+        name = n;
+        id = i;
+    }
+
     void showDetail()
     {
         cout << "ID of student is: " << id << endl;
@@ -28,8 +34,11 @@ public:
 
 int main()
 {
-    Student s1(4646932,"Asadullah");
+    Student s1, s2(4646932, "Asadullah"), s3("Bial", 897);
+
     s1.showDetail();
+    s2.showDetail();
+    s3.showDetail();
 
     return 0;
 }
