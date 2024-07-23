227100e8357cfb97ffa13b52576882af8c4bb2ca
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-07-23 11:45:33 +0500

    Memory Allocation in c++.

---
+++ memoryAllocation.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Shop
+{
+    int itemId[100];
+    int itemPrice[100];
+    int counter;
+
+public:
+    void initCounter(void) { counter = 0; }
+    void setPrice(void);
+    void displayPrice(void);
+};
+
+void Shop ::setPrice(void)
+{
+    cout << "Enter id of your item: " << counter + 1 << endl;
+    cin >> itemId[counter];
+    cout << "Enter price of your item: " << endl;
+    cin >> itemPrice[counter];
+    counter++;
+}
+void Shop ::displayPrice(void)
+{
+    for (int i = 0; i < counter; i++)
+    {
+        cout << "The price of your item id " << itemId[i] << " is " << itemPrice[i] << endl;
+    }
+}
+
+int main()
+{
+    Shop dukaan;
+    dukaan.initCounter();
+    dukaan.setPrice();
+    dukaan.setPrice();
+    dukaan.setPrice();
+    dukaan.displayPrice();
+
+    return 0;
+}
